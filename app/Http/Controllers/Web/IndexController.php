fa69c4ea16b1198818cc0559597fb9765a924fd9
hhy <1468018323@qq.com>
2017-11-12 09:48:17 +0800

    d

---
+++ app/Http/Controllers/Web/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+
+        return view('index', []);
+
+    }
+
+    public function upload()
+    {
+
+    }
+
+    public function doUpload()
+    {
+
+    }
+
+}
