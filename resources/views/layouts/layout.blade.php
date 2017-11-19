b23d9490d8417f60b1d5dfc8c799f8c5b124936e
hhy <1468018323@qq.com>
2017-11-19 22:48:19 +0800

    d

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,36 +1,67 @@
 <!DOCTYPE html>
-<html lang="zh-CN">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
-    <title>HHY'S WEBSITE</title>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Xenon Boostrap Admin Panel" />
+  <meta name="author" content="" />
+  
+  <title>HHY'S WEBSITE</title>
 
-    <!-- Bootstrap -->
-    <link href="/css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/zzsc.css" type="text/css" rel="stylesheet" />
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    
-    @section('sidebar')
-        
-    @show
+  
+  <link rel="stylesheet" href="/assets/css/fonts/linecons/css/linecons.css">
+  <link rel="stylesheet" href="/assets/css/fonts/fontawesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/assets/css/bootstrap.css">
+  <link rel="stylesheet" href="/assets/css/xenon-core.css">
+  <link rel="stylesheet" href="/assets/css/xenon-forms.css">
+  <link rel="stylesheet" href="/assets/css/xenon-components.css">
+  <link rel="stylesheet" href="/assets/css/xenon-skins.css">
+  <link rel="stylesheet" href="/assets/css/custom.css">
 
-    <div class="container">
-        @yield('content')
+  <script src="/assets/js/jquery-1.11.1.min.js"></script>
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  
+  
+</head>
+<body class="page-body">
+  
+  @include('layouts.header')
+  
+  <div class="page-container">
+
+    <div class="main-content">
+      
+      @yield('content')   
+      
+      @include('layouts.footer')
     </div>
-    <script src="/js/jquery.js"></script>
-    <script src="/js/layer-v3.1.0/layer/layer.js"></script>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="/js/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/js/bootstrap/js/bootstrap.min.js"></script>
-  </body>
+    
+  </div>
+  
+  
+  
+
+
+
+  <!-- Bottom Scripts -->
+  <script src="/assets/js/bootstrap.min.js"></script>
+  <script src="/assets/js/TweenMax.min.js"></script>
+  <script src="/assets/js/resizeable.js"></script>
+  <script src="/assets/js/joinable.js"></script>
+  <script src="/assets/js/xenon-api.js"></script>
+  <script src="/assets/js/xenon-toggles.js"></script>
+
+
+  <!-- JavaScripts initializations and stuff -->
+  <script src="/assets/js/xenon-custom.js"></script>
+  <script src="/js/layer-v3.1.0/layer/layer.js"></script>
+
+</body>
 </html>
