394c0bbb043f87c7e5bc8f99293c977281a58e26
hhy <1468018323@qq.com>
2017-12-10 22:25:58 +0800

    d

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,118 +1,59 @@
 @extends('layouts.layout')
 @section('content')
+<audio autoplay="autoplay" loop="-1" >
+	<source src="/js/music_two.mp3">
+</audio>
 <div class="row">
     <div class="col-md-12">
         
-        <!-- Default panel -->
-        <div class="panel panel-default">
-            
-            <div class="panel-body">
-				<audio autoplay="autoplay" loop="-1" >
-		    			<source src="/js/music_two.mp3">
-				</audio>
-				<script type="text/javascript">
-			// Sample Javascript code for this page
-			jQuery(document).ready(function($)
-			{
-				// Sample Select all images
-				$("#select-all").on('change', function(ev)
-				{
-					var is_checked = $(this).is(':checked');
-					
-					$(".album-image input[type='checkbox']").prop('checked', is_checked).trigger('change');
-				});
+	    <section class="gallery-env">
+			
+			<div class="row">
+			
+				<!-- Gallery Album Optipns and Images -->
+				<div class="col-sm-12 gallery-right">
 				
-				// Edit Modal
-				$('.gallery-env a[data-action="edit"]').on('click', function(ev)
-				{
-					ev.preventDefault();
-					$("#gallery-image-modal").modal('show');
-				});
-				
-				// Delete Modal
-				$('.gallery-env a[data-action="trash"]').on('click', function(ev)
-				{
-					ev.preventDefault();
-					$("#gallery-image-delete-modal").modal('show');
-				});
-				
-				
-				// Sortable
-				
-				$('.gallery-env a[data-action="sort"]').on('click', function(ev)
-				{
-					ev.preventDefault();
-					
-					var is_sortable = $(".album-images").sortable('instance');
-					
-					if( ! is_sortable)
-					{
-						$(".gallery-env .album-images").sortable({
-							items: '> div'
-						});
+					<!-- Album Images -->
+					<div class="album-images row ui-sortable"">
 						
-						$(".album-sorting-info").stop().slideDown(300);
-					}
-					else
-					{
-						$(".album-images").sortable('destroy');
-						$(".album-sorting-info").stop().slideUp(300);
-					}
-				});
-			});
-			</script>
-			
-			<section class="gallery-env">
-			
-				<div class="row">
-				
-					<!-- Gallery Album Optipns and Images -->
-					<div class="col-sm-9 gallery-right">
-					
-						<!-- Album Images -->
-						<div class="album-images row">
-							
-							<!-- Album Image -->
-							@if(!empty($photoes))
-								@foreach($photoes as $key => $value)
-									<div class="col-md-3 col-sm-4 col-xs-6">
-										<div class="album-image">
-											<a href="#" class="thumb" data-action="edit">
-												<img src="{{$value->path}}" class="img-responsive" />
-											</a>
-											
-											<a href="#" class="name">
-												<span>IMG_007.jpg</span>
-												<em>28 September 2014</em>
-											</a>
-											
-											<div class="image-options">
-												<a href="#" data-action="edit"><i class="fa-pencil"></i></a>
-												<a href="#" data-action="trash"><i class="fa-trash"></i></a>
-											</div>
-											
-											<div class="image-checkbox">
-												<input type="checkbox" class="cbr" />
-											</div>
+						<!-- Album Image -->
+						@if(!empty($photoes))
+							@foreach($photoes as $key => $value)
+								<div class="col-md-3 col-sm-4 col-xs-6 ui-sortable-handle">
+									<div class="album-image">
+										<a href="#" class="thumb" data-action="edit">
+											<img src="{{$value->path}}" class="img-responsive" />
+										</a>
+										
+										<a href="#" class="name">
+											<span>IMG_007.jpg</span>
+											<em>{{ date('Y-m-d', strtorime($value['created_at'])) }}</em>
+										</a>
+										
+										<div class="image-options">
+											<a href="#" data-action="edit"><i class="fa-pencil"></i></a>
+											<a href="#" data-action="trash"><i class="fa-trash"></i></a>
+										</div>
+										
+										<div class="image-checkbox">
+											<input type="checkbox" class="cbr" />
 										</div>
 									</div>
-								@endforeach
-						    @endif 
-						</div>
-						
-						
-						<button class="btn btn-white btn-block">
-							<i class="fa-bars"></i>
-							加载更多
-						</button>
-						
+								</div>
+							@endforeach
+					    @endif 
 					</div>
+					
+					
+					<button class="btn btn-white btn-block">
+						<i class="fa-bars"></i>
+						加载更多
+					</button>
+					
 				</div>
-				
-			</section>
-
 			</div>
-		</div>
+				
+		</section>
 	</div>
 </div>
 <script src="/js/index/index.js"></script>
